dfa6289114ec0c4e8226b24dacc5b6733889cbf6
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2023-03-11 12:31:18 +0530

    Polygon migration changes

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import { createAccountProfile } from './modules/wallet.js';
+import { createAccountProfile, mintTokens } from './modules/wallet.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,12 +22,24 @@ app.use(cors({
     origin: ['https://localhost:4000','https://192.168.0.112:4000']
 }));
 
-app.get("/create-acount-profile", async (req, res) => {
-    const wallet = await createAccountProfile("9962589489");
+app.get("/create-account-profile", async (req, res) => {
+    const externalAccountId = req.query.accountId;
+    const wallet = await createAccountProfile(externalAccountId);
 
     res.json({
         status: "Account created successfully",
         wallet,
+    });
+});
+
+app.get("/mint-tokens", async (req, res) => {
+    const externalAccountId = req.query.accountId;
+    const amount = req.query.amount;
+    console.log(`Minting tokens for ${externalAccountId}...`);
+    const result = await mintTokens(externalAccountId, amount);
+
+    res.json({
+        result,
     });
 });
 
--- server/src/modules/wallet.js
+++ server/src/modules/wallet.js
@@ -2,15 +2,24 @@ import ethers from "ethers";
 import crypto from "crypto";
 import dotenv from "dotenv";
 import { splitSignature } from "ethers/lib/utils.js";
-import { EXTERNAL_ACCOUNT_ID_WALLET_MAP, CONTRACT_ADDRESS, CONTRACT_ABI } from "../utils/constants.js";
+import { EXTERNAL_ACCOUNT_ID_WALLET_MAP, CONTRACT_ADDRESS, CONTRACT_ABI, DECIMALS } from "../utils/constants.js";
 import { getWeb3Instance } from "../utils/w3.js";
 
 dotenv.config();
+
+const getBigNumber = (number) => {
+  const web3 = getWeb3Instance();
+  return web3.utils.toBN(number);
+}
 
 const _storeWalletInVault = (externalAccountId, wallet) => {
     EXTERNAL_ACCOUNT_ID_WALLET_MAP[externalAccountId] = wallet;
     console.log(EXTERNAL_ACCOUNT_ID_WALLET_MAP);
 };
+
+const _getWalletFromVault = (externalAccountId) => {
+  return EXTERNAL_ACCOUNT_ID_WALLET_MAP[externalAccountId];
+}
 
 const createWallet = () => {
     const id = crypto.randomBytes(32).toString('hex');
@@ -21,28 +30,32 @@ const createWallet = () => {
 };
 
 const createAccountProfile = async (externalAccountId) => {
-    const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY);
     const { privateKey, wallet } = createWallet();
     const {
         address,
     } = wallet;
 
+    console.log("wallet", wallet);
+
     _storeWalletInVault(externalAccountId, { privateKey, address });
-    _provideTransferPermissionToSystemAccount(wallet, signer);
+    _provideTransferPermissionToSystemAccount(wallet);
     return wallet;
 };
 
-const _provideTransferPermissionToSystemAccount = async (wallet, signer) => {
-    const ownerAddress = wallet.address;
+const _provideTransferPermissionToSystemAccount = async (wallet) => {
+  const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY);
+  const ownerAddress = wallet.address;
     const spenderAddress = signer.address;
     const value = ethers.constants.MaxUint256; // Unlimited balance
-    const nonce = 0; // Since Permit is executed only once for a wallet
+    const nonce = 0; // Setting to 0 since Permit is executed only once for a wallet
     const deadline = ethers.constants.MaxUint256; // Unlimited timeframe
 
-    const web3 = getWeb3Instance();
+    const web3 = getWeb3Instance("POLYGON");
+
+    console.log("web3 instance created");
 
     const abi = CONTRACT_ABI;
-    const chainId = 5 // For Goerli
+    const chainId = process.env.CHAIN_ID;
     const name = "FoodToken";
     const version = "1";
 
@@ -142,6 +155,60 @@ const getPermitSignature = async (
     );
   }
 
+const mintTokens = async (externalAccountId, amount) => {
+  try {
+//    const wallet = _getWalletFromVault(externalAccountId);
+    const wallet = {
+      privateKey: '0x29577a4faeaf05421b93fdb882d3162c9ded82e8b24be4cd46823a05fee17f57',
+      address: '0xEEEDab9cAc42Ad100dBed36A1467ae545F83C6Fc'
+    };
+
+    const {
+      address,
+    } = wallet;
+
+    const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY);
+    const web3 = getWeb3Instance("POLYGON");
+
+    const chainId = process.env.CHAIN_ID;
+    // Call Permit method in contract
+    const contract = new web3.eth.Contract (
+      CONTRACT_ABI,
+      CONTRACT_ADDRESS,
+    );
+
+    const amountBN = getBigNumber(amount).mul(getBigNumber(DECIMALS));
+    const txn = contract.methods.mint(address, amountBN);
+    const gas = await txn.estimateGas({ from: signer.address });
+    const gasPrice = await web3.eth.getGasPrice();
+    const data = txn.encodeABI();
+    const nonce = await web3.eth.getTransactionCount(signer.address);
+
+    const signedTxn = await web3.eth.accounts.signTransaction({
+        to: contract.options.address,
+        data,
+        gas,
+        gasPrice,
+        nonce,
+        chainId,
+    }, process.env.SIGNER_PRIVATE_KEY);
+
+    const receipt = await web3.eth.sendSignedTransaction(signedTxn.rawTransaction);
+    console.log(receipt);
+    
+    return {
+      status: true,
+      message: "SUCCESS",
+    };
+  } catch (err) {
+    return {
+      status: false,
+      message: err.message,
+    };
+  }
+}
+
 export {
     createAccountProfile,
+    mintTokens,
 };
